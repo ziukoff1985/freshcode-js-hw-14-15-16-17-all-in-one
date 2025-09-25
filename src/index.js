95b1eae4391ff28792755318e32e32fded0222c6
Borys Ziukov <ziukoff1985@gmail.com>
2025-09-25 16:34:37 +0300

    Add container, form, h1, p

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const container = document.createElement('div');
+container.classList.add('container');
+document.body.append(container);
+
+const form = document.createElement('form');
+container.append(form);
+
+const h1 = document.createElement('h1');
+h1.textContent = 'CREATE AN ACCOUNT';
+form.append(h1);
+
+const formParagraph = document.createElement('p');
+formParagraph.textContent =
+    'We always keep your name and email address private';
+form.append(formParagraph);
