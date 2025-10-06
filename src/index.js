6f1edba2b2fdbc10bbe6095cfff34ec37e51cf77
Borys Ziukov <ziukoff1985@gmail.com>
2025-10-06 18:35:11 +0300

    Fix class and listener

--- src/index.js
+++ src/index.js
@@ -49,39 +49,89 @@ cancelButton.textContent = 'Cancel';
 btnWrapDiv.append(cancelButton);
 
 // DOM - events creation
+
 class Person {
-    constructor(
-        firstName = 'Not provided',
-        lastName,
-        nickName = 'Not provided',
-        email = 'Not provided'
-    ) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.nickName = nickName;
-        this.email = email;
+    constructor(...args) {
+        args.forEach(({ name, value }) => {
+            this[name] = value.trim();
+        });
     }
 }
 
 function onSubmitForm(event) {
     event.preventDefault();
-    const formElements = form.elements;
+    const formInputs = [...document.querySelectorAll('input')].filter(
+        ({ name, value }) => name && value.trim()
+    );
 
-    const person = new Person();
-
-    for (const { name, value, tagName } of formElements) {
-        if (name && tagName === 'INPUT' && value.trim()) {
-            person[name] = value.trim();
-        }
-    }
+    const person = new Person(...formInputs);
 
     if (!person.lastName) {
         console.log('Cannot save: Last Name is required and cannot be empty');
         return;
     }
 
-    localStorage.setItem(person.lastName, JSON.stringify(person));
+    const personJson = JSON.stringify(
+        person,
+        (key, value) => (key === 'email' ? undefined : value),
+        2
+    );
+
+    localStorage.setItem(person.lastName, personJson);
     form.reset();
 }
 
 form.addEventListener('submit', onSubmitForm);
+
+// class Person {
+//     constructor(
+//         firstName = 'Not provided',
+//         lastName,
+//         nickName = 'Not provided',
+//         email = 'Not provided'
+//     ) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.nickName = nickName;
+//         this.email = email;
+//     }
+// }
+
+// function onSubmitForm(event) {
+//     event.preventDefault();
+//     const formElements = form.elements;
+
+//     const person = new Person();
+
+//     for (const { name, value, tagName } of formElements) {
+//         if (name && tagName === 'INPUT' && value.trim()) {
+//             person[name] = value.trim();
+//         }
+//     }
+
+//     if (!person.lastName) {
+//         console.log('Cannot save: Last Name is required and cannot be empty');
+//         return;
+//     }
+
+//     localStorage.setItem(person.lastName, JSON.stringify(person));
+//     form.reset();
+// }
+
+// form.addEventListener('submit', onSubmitForm);
+
+// function onChangeInputs(event) {
+//     const { name, value } = event.target;
+
+//     console.log('Name:', name);
+
+//     console.log('Value:', value);
+// }
+
+// form.addEventListener('change', onChangeInputs);
+
+// const inputs = form.querySelectorAll('input');
+
+// inputs.forEach((input) => {
+//     input.addEventListener('input', onChangeInputs);
+// });
