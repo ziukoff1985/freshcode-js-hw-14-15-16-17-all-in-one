ea6586394cacf332eac4f697737abb57b3ec5335
Borys Ziukov <ziukoff1985@gmail.com>
2025-10-08 20:11:30 +0300

    Finally: before code review

--- src/validateEmail.js
+++ src/validateEmail.js
@@ -4,38 +4,30 @@ let isDataValid = false;
 
 export const getIsDataValid = () => isDataValid;
 
-export function setupDataValidation(
-    selector,
-    validationRegex,
-    errorMessageText
-) {
-    const emailInput = document.querySelector(selector);
-    if (!emailInput) return;
+export function setupDataValidation(selector, regexp, errorText) {
+    const input = document.querySelector(selector);
+    if (!input) return;
 
-    const inputGroupEmail = emailInput.closest('.input-group');
+    const inputGroup = input.closest('.input-group');
 
-    const emailRegex = validationRegex;
+    const regexpForValid = regexp;
 
-    const errorMessage = createNewElement(
-        'div',
-        'error-message',
-        inputGroupEmail
-    );
-    errorMessage.textContent = errorMessageText;
+    const errorMessage = createNewElement('div', 'error-message', inputGroup);
+    errorMessage.textContent = errorText;
 
     function validateData(event) {
-        const email = event.target.value;
-        const isValid = emailRegex.test(email);
+        const targetField = event.target.value;
+        const isValid = regexpForValid.test(targetField);
 
         isDataValid = isValid;
 
         if (!isValid) {
             errorMessage.classList.add('visible');
-            emailInput.classList.add('invalid');
+            input.classList.add('invalid');
         } else {
             errorMessage.classList.remove('visible');
-            emailInput.classList.remove('invalid');
+            input.classList.remove('invalid');
         }
     }
-    emailInput.addEventListener('input', validateData);
+    input.addEventListener('input', validateData);
 }
